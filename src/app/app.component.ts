94ed67ce595d1d91eb6d426bd9898dcee849e092
poreddyanusha030697 <137568557+poreddyanusha030697@users.noreply.github.com>
2023-09-03 20:37:35 -0400

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular_git';
+  loginUser(item:any){
+console.warn(item)
+  }
+}
